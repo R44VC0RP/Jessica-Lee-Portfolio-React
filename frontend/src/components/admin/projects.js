265e52689fbdc367c2188da90c9d2ee6c3343a47
R44VC0RP <n01508660@unf.edu>
2024-10-09 17:39:54 -0400

    for jess <3

--- frontend/src/components/admin/projects.js
+++ frontend/src/components/admin/projects.js
@@ -7,7 +7,7 @@ import { pdfjs } from 'react-pdf';
 import ReactDOM from 'react-dom';
 
 // Initialize pdfjs worker
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.min.js" integrity="sha512-Z8CqofpIcnJN80feS2uccz+pXWgZzeKxDsDNMD/dJ6997/LSRY+W4NmEt9acwR+Gt9OHN0kkI1CTianCwoqcjQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.mjs';
 
 // Generate the UploadDropzone and UploadButton components
 const UploadDropzone = generateUploadDropzone();
